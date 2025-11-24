61cf4d31dc3c88ee4e6876665b4b774d0e61d276
ProgrammerTon <pattarapol.nanetra@gmail.com>
2025-11-24 20:40:55 +0700

    add bootstrap

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './styles/fonts.css';
 import './styles/colors.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <h1 className='font-sarabun text-primary'>Hello Best 2x2 with React Yay!</h1>
+      {/* ใช้ Component จาก react-bootstrap */}
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Best 2x2 App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">หน้าแรก</Nav.Link>
+              <Nav.Link href="#link">ฟีเจอร์</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* เนื้อหา */}
+      <div className='p-3'>
+        <h1 className='font-sarabun text-primary'>Hello Best 2x2 with React Yay!</h1>
+      </div>
     </div>
   );
 }
