d9188fce1f52e7fe8ce996e98d92b353e4de29c7
ProgrammerTon <pattarapol.nanetra@gmail.com>
2026-01-12 23:54:14 +0700

    hash password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 const router = express.Router();
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await User.findOne({ username });
-        if (user && user.password === password) { 
+        if (!user) return res.status(401).json({ message: "ข้อมูลไม่ถูกต้อง" });
+
+        // เปรียบเทียบรหัสที่รับมา กับรหัสที่เป็น Hash ในฐานข้อมูล
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
             res.json({ success: true, token: "secret-token-123" });
         } else {
-            res.status(401).json({ message: "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" });
+            res.status(401).json({ message: "ข้อมูลไม่ถูกต้อง" });
         }
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -19,9 +25,18 @@ router.post('/login', async (req, res) => {
 router.post('/register', async (req, res) => {
     try {
         const { username, password } = req.body;
-        const newAdmin = new User({ username, password });
+        
+        // สร้าง Salt และทำการ Hash รหัสผ่าน (เลข 10 คือความละเอียดในการเข้ารหัส)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newAdmin = new User({ 
+            username, 
+            password: hashedPassword // บันทึกตัวที่ Hash แล้วลง MongoDB
+        });
+        
         await newAdmin.save();
-        res.status(201).json({ message: "สร้างแอดมินสำเร็จ!" });
+        res.status(201).json({ message: "สร้างแอดมินสำเร็จ" });
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
